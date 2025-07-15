25d65a1b91030df20859d05abac87f5efe43af3f
Nerbil <perry@perrywayman.com>
2025-07-15 20:12:31 +0100

    A: Draw screen and allow quitting

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+from constants import *
+
+number = 0
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((1280, 720))
+    while number == 0:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
