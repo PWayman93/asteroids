f70d637fced08fea5178ba21c66f48ceb36d2ecb
Nerbil <perry@perrywayman.com>
2025-07-18 17:02:48 +0100

    C: Added asteroids that move

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 clock = pygame.time.Clock()
 
@@ -12,7 +14,19 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (updateable, drawable, asteroids)
+    AsteroidField.containers = (updateable)
+    
     player = Player(640, 360)
+    
+    asteroid_field = AsteroidField()
+    
     
     screen = pygame.display.set_mode((1280, 720))
     
@@ -24,8 +38,9 @@ def main():
         dt = clock.tick(60) / 1000
                 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updateable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         
         
